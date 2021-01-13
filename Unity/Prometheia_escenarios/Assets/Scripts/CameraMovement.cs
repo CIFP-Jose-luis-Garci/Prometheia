1b19b896de105a5a94ff6cd8ef6dd3dc507c7672
Josusu <73643418+Josusu@users.noreply.github.com>
2021-01-13 20:37:55 +0100

    Escenarios y movimiento de la cámara
    Esto son las cosas que llevo hechas. Por cierto si no se ven bien los materiales o se ve todo negro es porque tengo descargado un render 2d llamado Universal en el package manager

---
+++ Unity/Prometheia_escenarios/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    [SerializeField] Transform Target;
+    Vector3 camaraVelocity = Vector3.zero;
+    [SerializeField] float smoothVelocity = 0.1f;
+    
+
+    //Variables limites
+    private float leftLimit, rightLimit, topLimit, bottonLimit;
+
+   
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+        Vector3 targetPosition = new Vector3(Target.position.x, Target.position.y + 1f, transform.position.z);     
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity,
+           smoothVelocity);
+
+
+        if (targetPosition.x > -10f && targetPosition.x < 97f && targetPosition.y < 4.9f)
+        {
+            transform.position = new Vector3
+            (
+                Mathf.Clamp(transform.position.x, leftLimit = 0f, rightLimit = 96.06f),
+                Mathf.Clamp(transform.position.y, bottonLimit = 0f, topLimit = 0f),
+                transform.position.z
+            );
+        }else if(targetPosition.x > 85.83f && targetPosition.x < 106.24f && targetPosition.y < 17f)
+        {
+            transform.position = new Vector3
+            (
+                Mathf.Clamp(transform.position.x, leftLimit = 96.06f, rightLimit = 96.06f),
+                Mathf.Clamp(transform.position.y, bottonLimit = 0f, topLimit = 20.28f),
+                transform.position.z
+            );
+
+        }else if (targetPosition.x > 85.83f && targetPosition.x < 163f && targetPosition.y > 17f)
+        {
+            transform.position = new Vector3
+            (
+                Mathf.Clamp(transform.position.x, leftLimit = 85.83f, rightLimit = 153.66f),
+                Mathf.Clamp(transform.position.y, bottonLimit = 20.28f, topLimit = 20.28f),
+                transform.position.z
+            );
+
+        }else if (targetPosition.x > 144.5f && targetPosition.x < 163f && targetPosition.y > 4.9f)
+        {
+            transform.position = new Vector3
+            (
+                Mathf.Clamp(transform.position.x, leftLimit = 153.66f, rightLimit = 153.66f),
+                Mathf.Clamp(transform.position.y, bottonLimit = 0f, topLimit = 20.28f),
+                transform.position.z
+            );
+
+        }else if (targetPosition.x > 144.5f && targetPosition.x < 240.5f && targetPosition.y < 4.9f)
+        {
+            transform.position = new Vector3
+            (
+                Mathf.Clamp(transform.position.x, leftLimit = 153.66f, rightLimit = 230.45f),
+                Mathf.Clamp(transform.position.y, bottonLimit = 0f, topLimit = 0f),
+                transform.position.z
+            );
+
+        }else if (targetPosition.x > 220f && targetPosition.x < 240.5f && targetPosition.y > 4.9f)
+        {
+            transform.position = new Vector3
+            (
+                Mathf.Clamp(transform.position.x, leftLimit = 230.45f, rightLimit = 230.45f),
+                Mathf.Clamp(transform.position.y, bottonLimit = 0f, topLimit = 43.5f),
+                transform.position.z
+            );
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Unity/Prometheia_escenarios/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float moveSpeed = 36f;
+    private Transform theObject;
+    public Vector2 hRange = Vector2.zero;
+    public Vector2 vRange = Vector2.zero;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverNave();  
+    }
+
+    void MoverNave()
+    {
+        moveSpeed = 6f;
+        /*
+        //Ejemplos de Input que podemos usar más adelante
+        if(Input.GetKey(KeyCode.Space))
+        {
+        print(“Se está pulsando”);
+        }
+        if(Input.GetButtonDown(“Fire1”))
+        {
+        print(“Se está disparando”);
+        }
+        */
+        //Variable float que obtiene el valor del eje horizontal y vertical
+        float desplX = Input.GetAxis("Horizontal");
+        float desplY = Input.GetAxis("Vertical");
+        //Restringimos los limites de la pantalla
+      
+        //Movemos la nave mediante el método transform.translate
+        //Lo multiplicamos por deltaTime, el eje y la velocidad de movimiento la nave
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * desplX);
+        transform.Translate(Vector3.up * Time.deltaTime * moveSpeed * desplY);
+
+    }
+}
